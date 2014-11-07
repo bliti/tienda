166e5c5463b47b5c961b6cc9f525a0c0bbf1f6b2
bliti <pablo.rivera.programmer@gmail.com>
2014-11-06 23:40:26 -0400

    working on product ordering process

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import decimal
 
 
 class Product(models.Model):
@@ -25,19 +26,37 @@ class Product(models.Model):
     through a video.
     
     Images are not required by default. Due to how
-    no every product requires an image. 
+    no every product requires an image.
+    
+    Price and shipping are in cents. This to comply with how stripe.com
+    handles charges. Example: 24.95 would be 2495.
     """
     sku = models.IntegerField(unique=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.IntegerField()
     quantity = models.IntegerField()
-    shipping = models.DecimalField(max_digits=6, decimal_places=2)
+    shipping = models.IntegerField()
     name = models.CharField(max_length=140)
     description = models.TextField() 
     ships_to = models.TextField()
-    image = models.TextField(null=True)
+    image = models.TextField(null=True, blank=True)
     serve_image_from_static = models.BooleanField(default=False)
-    video_link = models.URLField(null=True)
+    video_link = models.URLField(null=True, blank=True)
 
 
     def __unicode__(self):
         return self.name
+
+
+    @property
+    def price_with_shipping(self):
+        return self.price + self.shipping
+    
+    
+    @property
+    def price_as_decimal(self):
+        return decimal.Decimal(self.price) / 100
+    
+    
+    @property
+    def shipping_as_decimal(self):
+        return decimal.Decimal(self.shipping) / 100
--- product/tests.py
+++ product/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.db import IntegrityError
-from decimal import Decimal, InvalidOperation
 from .models import Product
 
 
@@ -11,43 +10,24 @@ class ProductTest(TestCase):
         
         self.product = Product.objects.create(
             sku=001,
-            price=29.97,
+            price=2997,
             quantity=1,
-            shipping=5.99,
+            shipping=599,
             name='Test Product',
             description='Its a test product.',
             ships_to='USA',
             image='http://sunny95.com/wp-content/blogs.dir/16/files/2013/05/rick-astley.jpg',
             video_link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
             )
-        
-        self.wrong_price = 9999999.999
-        self.wrong_shipping = 99999999.9999
-    
-    
-    def set_wrong_price(self):
-        self.product.price = self.wrong_price
-        try:
-            self.product.save()
-        except InvalidOperation:
-            return False
-    
-    
-    def set_wrong_shipping(self):
-        self.product.price = self.wrong_shipping
-        try:
-            self.product.save()
-        except InvalidOperation:
-            return False
-    
+            
     
     def create_product_same_sku_as_other_returns_false(self):
         try:
             self.product_copy = Product.objects.create(
             sku=001,
-            price=29.97,
+            price=2997,
             quantity=1,
-            shipping=5.99,
+            shipping=599,
             name='Test Product',
             description='Its a test product.',
             ships_to='USA'
@@ -55,14 +35,6 @@ class ProductTest(TestCase):
         except IntegrityError:
             return False
          
-    
-    def test_set_wrong_price_returns_false(self):
-        self.assertFalse(self.set_wrong_price())
-         
-    
-    def test_set_wrong_shipping_returns_false(self):
-        self.assertFalse(self.set_wrong_shipping())
-
 
     def test_product_sku_uniqueness(self):
         self.assertFalse(self.create_product_same_sku_as_other_returns_false())
